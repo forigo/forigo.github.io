879385d57449ea0ecd2143ca5519dbe983bde551
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 13:16:35 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,33 +2,36 @@ var app = angular.module("App", ["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider
+    .when('/', {
+        templateUrl: 'templates/home.html'
+    });
     .when('/sobre', {
         templateUrl: 'templates/sobre.html'
-    })
+    });
     .when('/projetos', {
         templateUrl: 'templates/projetos.html'
-    })
+    });
     .when('/gustavolimaadvocacia', {
         templateUrl: 'templates/projetos/gustavolimaadvocacia.html'
-    })
+    });
     .when('/sgnirts', {
         templateUrl: 'templates/projetos/sgnirts.html'
-    })
+    });
     .when('/rocketseat', {
         templateUrl: 'templates/projetos/rocketseat.html'
-    })
+    });
     .when('/certificados', {
         templateUrl: 'templates/certificados.html'
-    })
+    });
     .when('/contato', {
         templateUrl: 'templates/contato.html'
-    })
+    });
     .when('/obrigado', {
         templateUrl: 'templates/obrigado.html'
-    })
+    });
     .when('/404', {
         templateUrl: 'templates/404.html'
-    })
+    });
 
     .otherwise({redirectTo : '404'});
 
