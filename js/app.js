afd3ac8e9cbcf4076059add740e781b8a3b747eb
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 02:06:26 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,36 +3,36 @@ var app = angular.module("App", ["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider
     .when("https://forigo.github.io", {
-        templateUrl: "/templates/home.html"
+        templateUrl: "https://forigo.github.io/templates/home.html"
     })
-    .when("/sobre", {
-        templateUrl: "templates/sobre.html"
+    .when("https://forigo.github.io/sobre", {
+        templateUrl: "https://forigo.github.io/templates/sobre.html"
     })
-    .when("/projetos", {
-        templateUrl: "templates/projetos.html"
+    .when("https://forigo.github.io/projetos", {
+        templateUrl: "https://forigo.github.io/templates/projetos.html"
     })
-    .when("/gustavolimaadvocacia", {
-        templateUrl: "templates/projetos/gustavolimaadvocacia.html"
+    .when("https://forigo.github.io/gustavolimaadvocacia", {
+        templateUrl: "https://forigo.github.io/templates/projetos/gustavolimaadvocacia.html"
     })
-    .when("/sgnirts", {
-        templateUrl: "templates/projetos/sgnirts.html"
+    .when("https://forigo.github.io/sgnirts", {
+        templateUrl: "https://forigo.github.io/templates/projetos/sgnirts.html"
     })
-    .when("/rocketseat", {
-        templateUrl: "templates/projetos/rocketseat.html"
+    .when("https://forigo.github.io/rocketseat", {
+        templateUrl: "https://forigo.github.io/templates/projetos/rocketseat.html"
     })
-    .when("/certificados", {
-        templateUrl: "templates/certificados.html"
+    .when("https://forigo.github.io/certificados", {
+        templateUrl: "https://forigo.github.io/templates/certificados.html"
     })
-    .when("/contato", {
-        templateUrl: "templates/contato.html"
+    .when("https://forigo.github.io/contato", {
+        templateUrl: "https://forigo.github.io/templates/contato.html"
     })
-    .when("/obrigado", {
-        templateUrl: "templates/obrigado.html"
+    .when("https://forigo.github.io/obrigado", {
+        templateUrl: "https://forigo.github.io/templates/obrigado.html"
     })
-    .when("/404", {
-        templateUrl: "templates/404.html"
+    .when("https://forigo.github.io/404", {
+        templateUrl: "https://forigo.github.io/templates/404.html"
     })
 
-    .otherwise({redirectTo : '404'});
+    .otherwise({redirectTo : 'https://forigo.github.io/404'});
 
 });
