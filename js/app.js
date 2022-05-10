2a6c885307f2122daa093ed8d93dce0fb52bb28a
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 12:59:58 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,34 +3,34 @@ var app = angular.module("App", ["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider
     .when("https://forigo.github.io/", {
-        templateUrl: "templates/home.html"
+        templateUrl: "https://forigo.github.io/templates/home.html"
     })
     .when("https://forigo.github.io/sobre", {
-        templateUrl: "templates/sobre.html"
+        templateUrl: "https://forigo.github.io/templates/sobre.html"
     })
     .when("https://forigo.github.io/projetos", {
-        templateUrl: "templates/projetos.html"
+        templateUrl: "https://forigo.github.io/templates/projetos.html"
     })
     .when("https://forigo.github.io/gustavolimaadvocacia", {
-        templateUrl: "templates/projetos/gustavolimaadvocacia.html"
+        templateUrl: "https://forigo.github.io/templates/projetos/gustavolimaadvocacia.html"
     })
     .when("https://forigo.github.io/sgnirts", {
-        templateUrl: "templates/projetos/sgnirts.html"
+        templateUrl: "https://forigo.github.io/templates/projetos/sgnirts.html"
     })
     .when("https://forigo.github.io/rocketseat", {
-        templateUrl: "templates/projetos/rocketseat.html"
+        templateUrl: "https://forigo.github.io/templates/projetos/rocketseat.html"
     })
     .when("https://forigo.github.io/certificados", {
-        templateUrl: "templates/certificados.html"
+        templateUrl: "https://forigo.github.io/templates/certificados.html"
     })
     .when("https://forigo.github.io/contato", {
-        templateUrl: "templates/contato.html"
+        templateUrl: "https://forigo.github.io/templates/contato.html"
     })
     .when("https://forigo.github.io/obrigado", {
-        templateUrl: "templates/obrigado.html"
+        templateUrl: "https://forigo.github.io/templates/obrigado.html"
     })
     .when("https://forigo.github.io/404", {
-        templateUrl: "templates/404.html"
+        templateUrl: "https://forigo.github.io/templates/404.html"
     })
 
     .otherwise({redirectTo : '404'});
