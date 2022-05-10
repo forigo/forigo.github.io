b19d17180d2755dc6298b56587f69ceb591764a1
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 13:05:01 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("App", ["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider
-    .when("https://forigo.github.io/", {
+    .when("/", {
         templateUrl: "/templates/home.html"
     })
     .when("sobre", {
