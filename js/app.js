1fcbeeae9e1fc111bf3ecb59ebdfd51352585c76
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 18:26:17 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@ app.config(function($routeProvider, $locationProvider){
     .when('/projetos', {
         templateUrl: 'templates/projetos.html'
     })
-    .when('projetos/gustavolimaadvocacia', {
+    .when('/projetos/gustavolimaadvocacia', {
         templateUrl: 'templates/projetos/gustavolimaadvocacia.html'
     })
-    .when('projetos/sgnirts', {
+    .when('/projetos/sgnirts', {
         templateUrl: 'templates/projetos/sgnirts.html'
     })
-    .when('projetos/rocketseat', {
+    .when('/projetos/rocketseat', {
         templateUrl: 'templates/projetos/rocketseat.html'
     })
     .when('/certificados', {
