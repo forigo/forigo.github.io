bbd571185abeab2b63057876925dd7a38c76444d
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 16:54:13 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,34 +4,34 @@ app.config(function($routeProvider){
     $routeProvider
     .when('/', {
         templateUrl: 'https://forigo.github.io/templates/home.html'
-    });
+    })
     .when('/sobre', {
         templateUrl: 'templates/sobre.html'
-    });
+    })
     .when('/projetos', {
         templateUrl: 'templates/projetos.html'
-    });
+    })
     .when('/gustavolimaadvocacia', {
         templateUrl: 'templates/projetos/gustavolimaadvocacia.html'
-    });
+    })
     .when('/sgnirts', {
         templateUrl: 'templates/projetos/sgnirts.html'
-    });
+    })
     .when('/rocketseat', {
         templateUrl: 'templates/projetos/rocketseat.html'
-    });
+    })
     .when('/certificados', {
         templateUrl: 'templates/certificados.html'
-    });
+    })
     .when('/contato', {
         templateUrl: 'templates/contato.html'
-    });
+    })
     .when('/obrigado', {
         templateUrl: 'templates/obrigado.html'
-    });
+    })
     .when('/404', {
         templateUrl: 'templates/404.html'
-    });
-    .otherwise({redirectTo : '404'});
+    })
+    .otherwise({redirectTo : '404'})
     
 });
