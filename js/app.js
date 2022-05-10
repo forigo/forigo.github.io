ea2bc070288d0edc6e517d0aef3278af1c698c48
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 17:28:55 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module("App", ["ngRoute"]);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
     $routeProvider
     .when('/', {
         templateUrl: 'templates/home.html'
