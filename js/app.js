a66829fa351bed7a2b0c71f4aadcb59ad70f472a
Felipe Forigo [sgnirts] <95830636+forigo@users.noreply.github.com>
2022-05-10 12:58:34 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,34 +2,34 @@ var app = angular.module("App", ["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider
-    .when("/", {
+    .when("https://forigo.github.io/", {
         templateUrl: "templates/home.html"
     })
-    .when("/sobre", {
+    .when("https://forigo.github.io/sobre", {
         templateUrl: "templates/sobre.html"
     })
-    .when("/projetos", {
+    .when("https://forigo.github.io/projetos", {
         templateUrl: "templates/projetos.html"
     })
-    .when("/gustavolimaadvocacia", {
+    .when("https://forigo.github.io/gustavolimaadvocacia", {
         templateUrl: "templates/projetos/gustavolimaadvocacia.html"
     })
-    .when("/sgnirts", {
+    .when("https://forigo.github.io/sgnirts", {
         templateUrl: "templates/projetos/sgnirts.html"
     })
-    .when("/rocketseat", {
+    .when("https://forigo.github.io/rocketseat", {
         templateUrl: "templates/projetos/rocketseat.html"
     })
-    .when("/certificados", {
+    .when("https://forigo.github.io/certificados", {
         templateUrl: "templates/certificados.html"
     })
-    .when("/contato", {
+    .when("https://forigo.github.io/contato", {
         templateUrl: "templates/contato.html"
     })
-    .when("/obrigado", {
+    .when("https://forigo.github.io/obrigado", {
         templateUrl: "templates/obrigado.html"
     })
-    .when("/404", {
+    .when("https://forigo.github.io/404", {
         templateUrl: "templates/404.html"
     })
 
